95468013c75c98c6ef7f900e8ba8a7386aeef71d
Justin Marozas <justinas.marozas@gmail.com>
2023-08-24 16:39:59 +0100

    register a smaller user model for admin page

--- djitter/admin.py
+++ djitter/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import User, Group
+
+
+class UserAdmin(admin.ModelAdmin):  # type: ignore
+    models = User
+    fields = ['username']
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.unregister(Group)
