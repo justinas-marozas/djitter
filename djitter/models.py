c2ca0d03fb7f9ee14468c13c52d496773a14b2b9
Justin Marozas <justinas.marozas@gmail.com>
2023-08-25 09:40:12 +0100

    add user profile model

--- djitter/models.py
+++ djitter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # type: ignore
+    follows = models.ManyToManyField(  # type: ignore
+        'self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True,
+    )
