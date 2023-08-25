c57ada3842d8c8c131f923c9b8514bb709df8e87
Justin Marozas <justinas.marozas@gmail.com>
2023-08-25 09:51:45 +0100

    add user profile to django admin

--- djitter/admin.py
+++ djitter/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
+from djitter.models import Profile
+
+
+class ProfileInline(admin.StackedInline):  # type: ignore
+    model = Profile
+
 
 class UserAdmin(admin.ModelAdmin):  # type: ignore
     models = User
     fields = ['username']
+    inlines = [ProfileInline]
 
 
 admin.site.unregister(User)
